9329ba7f034b8bc399fae3af019aed3775542ca2
Chathura Sudaraka <Chathurabro68@gmail.com>
2025-04-11 12:26:55 +0530

    Update

--- 404.php
+++ 404.php
@@ -1,12 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>404 Not Found</title>
+    <title>404 - Page Not Found | LaRosa - Wedding & Event Planning</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicon.png">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="/assets/css/preloader.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/meanmenu.css">
+    <link rel="stylesheet" href="/assets/css/animate.min.css">
+    <link rel="stylesheet" href="/assets/css/cursor.css">
+    <link rel="stylesheet" href="/assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/assets/css/swiper-bundle.css">
+    <link rel="stylesheet" href="/assets/css/backToTop.css">
+    <link rel="stylesheet" href="/assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="/assets/css/nice-select.css">
+    <link rel="stylesheet" href="/assets/css/fontAwesome5Pro.css">
+    <link rel="stylesheet" href="/assets/css/flaticon.css">
+    <link rel="stylesheet" href="/assets/css/default.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
+<!-- breadcrumb area start -->
+
 <body>
-    <h1>404 Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
+    <div class="lv-breadcrumb-area has-breadcrumb-overlay bg-default pt-205 pb-200 mb-120"
+        style="background-image: url('/assets/img/breadcrumb/breadcrumb-bg.jpg');">
+        <div class="container">
+            <div class="row">
+                <div class="col-xxl-12">
+                    <div class="lv-breadcrumb-content">
+                        <h4 class="lv-breadcrumb-title text-center">404 Not Found</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb area end -->
+
+    <!-- error area start -->
+    <div class="error-area pb-120">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-8 text-center">
+                    <div class="error-content">
+                        <h1 class="error-title">404</h1>
+                        <h2 class="error-subtitle">Oops! Page Not Found</h2>
+                        <p class="error-text mb-35">The page you are looking for might have been removed, had its name
+                            changed, or
+                            is temporarily unavailable.</p>
+                        <div class="lv-about-action">
+                            <a href="/" class="lv-theme-btn">
+                                <span class="lv-theme-horizontal-line"></span>
+                                <span class="lv-theme-vertical-line"></span>
+                                <span>Back to Homepage</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- error area end -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="/assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/meanmenu.js"></script>
+    <script src="/assets/js/swiper-bundle.min.js"></script>
+    <script src="/assets/js/magnific-popup.min.js"></script>
+    <script src="/assets/js/jarallax.min.js"></script>
+    <script src="/assets/js/appair.min.js"></script>
+    <script src="/assets/js/odometer.min.js"></script>
+    <script src="/assets/js/jquery-ui-slider-range.js"></script>
+    <script src="/assets/js/cursor.js"></script>
+    <script src="/assets/js/backToTop.js"></script>
+    <script src="/assets/js/nice-select.min.js"></script>
+    <script src="/assets/js/ajax-form.js"></script>
+    <script src="/assets/js/wow.min.js"></script>
+    <script src="/assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="/assets/js/isotope.pkgd.min.js"></script>
+    <script src="/assets/js/main.js"></script>
 </body>
-</html>
